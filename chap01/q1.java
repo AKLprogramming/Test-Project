198ec4977191942108a557cac957574786ff4167
AKLProgrammer <emailangkokleong@gmail.com>
2017-12-22 13:41:34 +0800

    q1v3

--- chap01/q1.java
+++ chap01/q1.java
@@ -1,50 +1,62 @@
 import java.util.*;
 import java.lang.Math;
 
-public class q1{
-	
-	public static void main(String[] args){
-		
+public class q1 {
+
+	public static void main(String[] args) {
 		System.out.println("Key in an integer: ");
-		
-		int test = 0;
-		int i =0;
-		while(i < test){
-			i=i+8;
-			test = test - i;
+
+		Scanner sc = new Scanner(System.in);
+
+		int test = sc.nextInt();
+		// binary
+		// octal
+		System.out.println("Binary: " + Integer.toBinaryString(test));
+		System.out.println("Octal: " + Integer.toOctalString(test));
+	}
+
+	public static double power(double compare, double num) {
+		if (compare < Math.pow(8, num)) {
+			return num - 1;
+		} else {
+			return power(compare, (num + 1));
+		}
+	}
+
+	public static String octal(double integer) {
+
+		String text = "";
+		double result = 0;
+		double power = power(integer, 0.0);
+		double answer = 0;
+		double ans = 0.0;
+
+		while (true) {
+			
+			for (int j = 0; j < 100; j++) {
+				if ((integer - (Math.pow(8, power) * j)) < 0) {
+					answer = j - 1;
+					result = integer - (Math.pow(8, power) * (answer));
+					break;
+				}
+			}
+			
+			ans = (integer % result);
+			text = Double.toString(answer);
+			
+			if (integer < Math.pow(8, power)) {
+				
+			} else {
+				
+			}
+			break;
 		}
 		
-		
-		System.out.println(octal(24));
+		return text;
+		/*
+		 * if (integer < Math.pow(8, power)) return Double.toString(power); else
+		 * return text + octal(ans);
+		 */
 		
 	}
-	
-	public static string octal(int integer){
-		string text = "";
-		int result = 0;		
-		int power = 0;
-		for(int i =0; i<200; i++){
-			if(integer < Math.pow(8,i)){
-				//note: obtain the smallest difference for 
-				//result
-				power = i-1;
-				result = (integer % Math.pow(8, power));
-				break; 		
-			}
-		}
-		
-		int result2 = 0;
-		int answer = 0;
-		
-		for(int j =0; j<100; j++){
-			if((integer-(Math.pow(8, power)*j)) < 0){
-				answer = j-1;				
-				result2 = integer-(Math.pow(8, power)*(answer));
-			}
-			break;	
-		}	
-		
-		return answer.toString() + octal(result2);
-	}
-	
 }
