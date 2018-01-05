4bf2acf0e2e484ebf476d284bddff957647045b5
AKLProgrammer <emailangkokleong@gmail.com>
2018-01-05 17:28:02 +0800

    completed coinvalueconversion

--- C++/CoinValueConversion.cpp
+++ C++/CoinValueConversion.cpp
@@ -5,7 +5,31 @@ using namespace std;
 
 int main(){
 
+    //quarters = 25 cents
+    //dime = 10 cents
+    //nickels = 5 cents
+    //pennies = 1 cent
+
+
+    double convertQuartersToPennies = 25 * 1;
+    double convertDimeToPennies = 10 * 1;
+    double convertNickelsToPennies = 5 * 1;
+    double pennies = 1;
     
+    int option = 0;
+
+    cout << "Choose the type of coin you want to convert to pennies " << endl;
+    cout << "1. Quarters" << endl;
+    cout << "2. Dime" << endl;
+    cout << "3. Nickels" << endl;
+    cout << "4. Pennies" << endl;
+    cout << "Select :" ;
+    cin >> option;
+
+    cout << "Select the amount of "
+
+    if(option == 1)
+        
 
     return 0;
 }
